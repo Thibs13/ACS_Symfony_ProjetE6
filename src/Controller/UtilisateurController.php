0fb5faf5f61f7b753c76538928963a4c5fa0985b
Thibs13 <caumontthibault1313@gmail.com>
2026-03-16 11:50:45 +0100

    Utilisateurs Sécurisé

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -6,18 +6,22 @@ use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class UtilisateurController extends AbstractController
 {
     #[Route('/utilisateurs', name: 'app_utilisateurs')]
-    public function index(UtilisateurRepository $utilisateurRepository): Response
+    public function index(RequestStack $requestStack): Response
     {
-        // On récupère tous les utilisateurs depuis la base de données
-        $utilisateurs = $utilisateurRepository->findAll();
+        $session = $requestStack->getSession();
+        $userSession = $session->get('user');
 
+        if (!$userSession) {
+            return $this->redirectToRoute('app_accueil');
+        }
         // On retourne la vue en lui passant le tableau d'utilisateurs
         return $this->render('utilisateur/index.html.twig', [
-            'utilisateurs' => $utilisateurs,
+            'utilisateurs' => $userSession,
         ]);
     }
 }
