5927bcfab7864d3dcf621039dbca9459403c9ddc
Maxence Allard <maxence.allard.76@gmail.com>
2026-03-19 08:26:20 +0100

    Update StageType.php
    Renommage Ajout Stage

--- src/Form/StageType.php
+++ src/Form/StageType.php
@@ -16,29 +16,33 @@ class StageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('STA_DateDebut')
-            ->add('STA_DateFin')
-            ->add('STA_Remarque')
-            ->add('STA_Remerciement')
-            ->add('STA_Bilan')
-            ->add('STA_Attestation')
-            ->add('STA_Jury')
-            ->add('STA_Commentaire')
-            ->add('STA_DateRetenu')
+            ->add('STA_DateDebut', null, ['label' => 'Date de début'])
+            ->add('STA_DateFin', null, ['label' => 'Date de fin'])
+            ->add('STA_Remarque', null, ['label' => 'Remarque'])
+            ->add('STA_Remerciement', null, ['label' => 'Remerciement'])
+            ->add('STA_Bilan', null, ['label' => 'Bilan'])
+            ->add('STA_Attestation', null, ['label' => 'Attestation'])
+            ->add('STA_Jury', null, ['label' => 'Jury'])
+            ->add('STA_Commentaire', null, ['label' => 'Commentaire'])
+            ->add('STA_DateRetenu', null, ['label' => 'Date retenu'])
             ->add('ETU_ID', EntityType::class, [
                 'class' => Etudiant::class,
+                'label' => 'Étudiant',
                 'choice_label' => 'id',
             ])
             ->add('ENT_ID', EntityType::class, [
                 'class' => Entreprise::class,
+                'label' => 'Entreprise',
                 'choice_label' => 'id',
             ])
             ->add('EnseignantVisite', EntityType::class, [
                 'class' => Utilisateur::class,
+                'label' => 'Enseignant de visite',
                 'choice_label' => 'id',
             ])
             ->add('EnseignantSuivi', EntityType::class, [
                 'class' => Utilisateur::class,
+                'label' => 'Enseignant de suivi',
                 'choice_label' => 'id',
             ])
         ;
