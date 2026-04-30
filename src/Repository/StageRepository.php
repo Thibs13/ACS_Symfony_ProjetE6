8eeee3d2b8e89a744e49adb0fe67651a838aa59c
Thibs13 <caumontthibault1313@gmail.com>
2026-04-30 08:39:35 +0200

    Update StageRepository.php

--- src/Repository/StageRepository.php
+++ src/Repository/StageRepository.php
@@ -53,14 +53,6 @@ class StageRepository extends ServiceEntityRepository
         {
             $qb = $this->createQueryBuilder('e');
 
-            // Cas spécifique : Tri par le NOM de la ville
-            if ($sort === 'ETU_NOM') {
-                return $qb->leftJoin('e.ETU_ID', 'v') // 'e.VIL_ID' est le nom de la propriété dans Entreprise.php
-                        ->orderBy('v.VIL_Nom', $order) // 'VIL_Nom' est le nom de la propriété dans Ville.php
-                        ->getQuery()
-                        ->getResult();
-            }
-
             // Cas général : Tri sur les champs directs (ex: ENT_Nom)
             // On ajoute un 'e.' devant pour être sûr que Doctrine sache de quoi on parle
             return $qb->orderBy('e.' . $sort, $order)
