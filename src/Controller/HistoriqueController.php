9ab92610b3d6d67c07f6a3ce1b3b25ca1c0a6ac3
Thibs13 <caumontthibault1313@gmail.com>
2026-05-04 11:57:19 +0200

    Update nav bar

--- src/Controller/HistoriqueController.php
+++ src/Controller/HistoriqueController.php
@@ -10,15 +10,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use \Symfony\Component\HttpFoundation\RequestStack; 
 
 #[Route('/historique')]
 final class HistoriqueController extends AbstractController
 {
     #[Route(name: 'app_historique_index', methods: ['GET'])]
-    public function index(HistoriqueRepository $historiqueRepository): Response
+    public function index(HistoriqueRepository $historiqueRepository, RequestStack $requestStack): Response
     {
+        $session = $requestStack->getSession();
+        $userSession = $session->get('user');
+
+        // si personne n'est connecté, on renvoie l'utilisateur vers la page de connexion
+        if (!$userSession) {
+            return $this->redirectToRoute('app_accueil');
+        }
+
         return $this->render('historique/index.html.twig', [
             'historiques' => $historiqueRepository->findAll(),
+            'role' => $userSession['role'] ?? 0,
         ]);
     }
 
