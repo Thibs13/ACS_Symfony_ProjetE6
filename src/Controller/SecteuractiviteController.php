b1b80204d1445c865837dacb5f8853dbe99e3dfc
Thibs13 <caumontthibault1313@gmail.com>
2026-05-07 09:10:15 +0200

    Update SecteuractiviteController.php

--- src/Controller/SecteuractiviteController.php
+++ src/Controller/SecteuractiviteController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Secteuractivite;
 use App\Form\SecteuractiviteType;
+use App\Entity\Historique;
+use App\Entity\Utilisateur;
+use DateTime;
 use App\Repository\SecteuractiviteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -51,6 +54,30 @@ final class SecteuractiviteController extends AbstractController
             $entityManager->persist($secteuractivite);
             $entityManager->flush();
 
+            // Partie pour les logs
+            $data = $form->getData();
+
+            foreach ($form->all() as $fieldName => $field) {
+                $value = $field->getData();
+
+                    $valeurAEnregistrer = (string)$value;
+
+                    $historique = new Historique();
+                    $historique->setHISDate(new DateTime());
+                    $historique->setHISNouvelleValeur($valeurAEnregistrer);
+                    $historique->setHISAncienneValeur('');
+
+                    $user = $entityManager->getRepository(Utilisateur::class)->find($userSession['id']);
+                    $historique->setUTIID($user);
+
+                    $entityManager->persist($historique);
+            }
+
+            // Un seul flush global pour tous les logs
+            $entityManager->flush();
+
+            //Fin partie pour les logs
+
             return $this->redirectToRoute('app_secteuractivite_read', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -90,9 +117,38 @@ final class SecteuractiviteController extends AbstractController
         }
 
         $form = $this->createForm(SecteuractiviteType::class, $secteuractivite);
+
+        // Utilisation d'un tableau associatif pour éviter les erreurs d'ordre
+        $anciennesValeurs = [
+            'secteur' => (string)$secteuractivite->getSaLibelle(),
+        ]; 
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $nouvellesValeurs = [
+                'secteur' => (string)$secteuractivite->getSaLibelle(),
+            ];
+
+            $user = $entityManager->getRepository(Utilisateur::class)->find($userSession['id']);
+            $dateLog = new DateTime();
+
+            foreach ($anciennesValeurs as $cle => $ancienneVal) {
+                $nouvelleVal = $nouvellesValeurs[$cle];
+
+                if ($ancienneVal !== $nouvelleVal) {
+                    $historique = new Historique();
+                    $historique->setHISDate($dateLog);
+                    $historique->setUTIID($user);
+                    $historique->setHISNouvelleValeur($nouvelleVal);
+                    $historique->setHISAncienneValeur($ancienneVal);
+                    
+                    // (Optionnel) Tu pourrais même enregistrer le nom du champ modifié avec $cle !
+
+                    $entityManager->persist($historique);
+                }
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_secteuractivite_read', [], Response::HTTP_SEE_OTHER);
@@ -106,9 +162,35 @@ final class SecteuractiviteController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_secteuractivite_delete', methods: ['POST'])]
-    public function delete(Request $request, Secteuractivite $secteuractivite, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, Secteuractivite $secteuractivite, EntityManagerInterface $entityManager, RequestStack $requestStack): Response
     {
+        // on récupère la session en cours pour vérifier qui navigue sur le site
+        $session = $requestStack->getSession();
+        $userSession = $session->get('user');
+
+        // si personne n'est connecté, on renvoie l'utilisateur vers la page de connexion
+        if (!$userSession OR $userSession['role'] != 1) {
+            return $this->redirectToRoute('app_accueil');
+        }
+
         if ($this->isCsrfTokenValid('delete'.$secteuractivite->getId(), $request->getPayload()->getString('_token'))) {
+            $user = $entityManager->getRepository(Utilisateur::class)->find($userSession['id']);
+            $dateLog = new DateTime();
+
+            $anciennesValeurs = [
+                (string)$secteuractivite->getSaLibelle(),
+            ];
+
+            foreach ($anciennesValeurs as $valeur) {
+                $historique = new Historique();
+                $historique->setHISDate($dateLog);
+                $historique->setUTIID($user);
+                $historique->setHISNouvelleValeur('');
+                $historique->setHISAncienneValeur($valeur);
+
+                $entityManager->persist($historique);
+            }
+
             $entityManager->remove($secteuractivite);
             $entityManager->flush();
         }
